8e73d60a3dac1221d553b1e1e71fdfb18eb753e2
Olexandr Humeniuk <thoron777@gmail.com>
2015-01-17 18:33:37 +0200

    Update Lecture_4_Min_max_problem.py

--- Lecture_4_Min_max_problem.py
+++ Lecture_4_Min_max_problem.py
@@ -3,6 +3,14 @@ hi = 10
 x = 12
 
 def clip(lo, x, hi):
+    '''
+    Takes in three numbers and returns a value based on the value of x.
+    Returns:
+     - lo, when x < lo
+     - hi, when x > hi
+     - x, otherwise
+    '''
+    # Your code here
     x = max (lo, x)
     x = min (x, hi)
     return x
